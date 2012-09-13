aaba19461966727a05662ac15dce165476ee463f
Dan Stoner <danstoner@gmail.com>
2012-09-13 12:25:22 -0400

    Add all options and required options to usage text.

--- image-resize-rename-forweb.pl
+++ image-resize-rename-forweb.pl
@@ -24,37 +24,67 @@ my $out_suffix = '';      # string to append to output filename (before extensio
 
 my $usage =
 '
-image-resize-rename-forweb.pl
+image-resize-rename-forweb.pl   - resize and rename images to meet specified pattern and size.
 
 Usage:
+
+  image-resize-rename-forweb.pl [options] -i dir1 -o dir2 -w dir3 --prefix=X --suffix=XXXXX 
+
+Options:
 
    -h, --help, --usage
           Display this usage help text.
    -v, --verbose
           Turn on verbose mode.
+
    -w, --width
-          Width of resized images in pixels
+          Width of resized images in pixels (default 600 px)
+   -x, --index_digits
+          The number of digits in the unique filename index (default 3 digits)
+
+Required Options:
+
    -i, --in_dir
           The directory containing raw input files.
    -o, --out_dir
           The directory for renamed output files.
    -w, --resized_out_dir
-          The directory for web resized output files. 
+          The directory for web resized output files.
+   --prefix
+          The prefix for each new filename
+   --suffix
+          The suffix to append to the output filename (before the extension)
 ';
 
 # check to see if sane options were passed
 
-print $usage;
+
+
+
+
+
+
+
+
+# if no args or usage or help passed, spit out...
+#if (
+  print $usage;
 
 
 # check to see if Image Magick convert command is installed.
 # 
 # convert --version
+my $convert = `convert --version`;
+if ($convert !~ /Version: ImageMagick/) 
+   {  print "$convert\n";
+      print "\n";
+      die 'ImageMagick "convert" command does not seem to be installed.';
+# Sample valid ImageMagick convert version output...
 # Version: ImageMagick 6.5.4-7 2012-04-10 Q16 OpenMP http://www.imagemagick.org
 # Copyright: Copyright (C) 1999-2009 ImageMagick Studio LLC
 
-
-
+   }
+else {print "Using...\n$convert\n";}
 # check to see if specified directories exist
 
 
