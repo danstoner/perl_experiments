5ef11bab27b35a959d829d56816762f74cf2632a
Dan Stoner <danstoner@gmail.com>
2012-09-13 10:55:45 -0400

    Standardize usage output

--- image-resize-rename-forweb.pl
+++ image-resize-rename-forweb.pl
@@ -14,12 +14,12 @@ my $help = '';            # help or usage (default = false)
 my $pixel_width = 600;    # width of output images (default = 600)
 my $index_digits = 3;     # the number of digits in the file index
 # options variables requiring a value
-my $in_dir = '';          # the directory containing raw input files.        REQUIRED
+my $in_dir = '';          # the directory containing raw input files. REQUIRED
 my $out_dir = '';         # the directory for renamed output files.   REQUIRED
 my $resized_out_dir = ''; # the directory for images resized for web. REQUIRED
-my $start_index = '';     # starting index for output files
-my $out_prefix = '';      # string to prepend to output filename
-my $out_suffix = '';      # string to append to output filename (before extension)
+my $start_index = '';     # starting index for output files           REQUIRED
+my $out_prefix = '';      # string to prepend to output filename      REQUIRED
+my $out_suffix = '';      # string to append to output filename (before extension) REQUIRED
 
 
 my $usage =
@@ -28,12 +28,18 @@ image-resize-rename-forweb.pl
 
 Usage:
 
-   --help | -h | --usage   Display this usage help text.
-   --verbose | -v          Turn on verbose mode.
-   --width | -w            Width of resized images in pixels
-   --in_dir    | -i        The directory containing raw input files.
-   --out_dir   | -o        The directory for renamed output files.
-   --resized_out_dir | -w  The directory for web resized output files. 
+   -h, --help, --usage
+          Display this usage help text.
+   -v, --verbose
+          Turn on verbose mode.
+   -w, --width
+          Width of resized images in pixels
+   -i, --in_dir
+          The directory containing raw input files.
+   -o, --out_dir
+          The directory for renamed output files.
+   -w, --resized_out_dir
+          The directory for web resized output files. 
 ';
 
 # check to see if sane options were passed
